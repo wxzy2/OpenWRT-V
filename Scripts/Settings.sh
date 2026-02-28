1072c77cbfa95819fda05a025f8d9c971c68f440
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2026-02-28 08:49:05 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -48,8 +48,8 @@ fi
 DTS_PATH="./target/linux/qualcommax/dts/"
 if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	#取消nss相关feed
-	#echo "CONFIG_FEED_nss_packages=n" >> ./.config
-	#echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
+	echo "CONFIG_FEED_nss_packages=n" >> ./.config
+	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
 	#开启sqm-nss插件
 	# echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
 	# echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
